5650db14230aaa28eb17c7f533dab5dd1f0c2349
Roman Lyakhovchik <roman.lyakhovchik@gmail.com>
2022-06-27 11:11:16 +0300

    asdf

--- bin/se-completion.sh
+++ bin/se-completion.sh
@@ -7,7 +7,6 @@ elif test -f ~/.zshrc
 then
     FRONT_PATH=$(grep "^$SE_FRONT_DIRECTORY=" ~/.zshrc | sed "s/$SE_FRONT_DIRECTORY=//g")
 fi
-
 SECTIONS_JSON_PATH="$FRONT_PATH/sections.json"
 
 _se_completions()
@@ -16,6 +15,8 @@ _se_completions()
 
     npm_command_options="build serve"
     npm_command="${COMP_WORDS[1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     if [[ ${COMP_CWORD} -eq 1 ]]
     then
@@ -23,9 +24,23 @@ _se_completions()
         return 0
     fi
 
-    frontend_sections_options=`grep -o '"[a-zA-Z0-9]*":[^{]*{' $SECTIONS_JSON_PATH | grep '[a-zA-Z0-9]' | tr -d '{ "\n' | sed 's/:/ /g'`
+    if [[ ${COMP_WORDS[COMP_CWORD]} == -* ]]
+    then
+        COMPREPLY=( $(compgen -W "-t -h --threads --help" -- ${cur}) )
+        return 0
+    fi
 
-    COMPREPLY=( $(compgen -W "${frontend_sections_options}" -- ${COMP_WORDS[COMP_CWORD]}) )
+    case $prev in
+        -h|--help)
+            return 0
+            ;;
+        -t|--threads)
+            return 0
+            ;;
+    esac
+
+    frontend_sections_options=`grep -o '"[a-zA-Z0-9]*":[^{]*{' $SECTIONS_JSON_PATH | grep '[a-zA-Z0-9]' | tr -d '{ "\n' | sed 's/:/ /g'`
+    COMPREPLY=( $(compgen -W "${frontend_sections_options}" -- ${cur}) )
 }
 
 complete -F _se_completions se
